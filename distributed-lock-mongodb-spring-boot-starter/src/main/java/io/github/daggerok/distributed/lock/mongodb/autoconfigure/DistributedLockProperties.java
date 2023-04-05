876b690f110bb70408c4c48175d8a38d7e36fbbd
Maksim Kostromin <maksim@beachfront.com>
2023-04-05 11:02:22 -0400

    Merge changes from spring-boot-2.7.x branch and do necessary fixes. Update project version to: 2.6.14-SNAPSHOT

--- distributed-lock-mongodb-spring-boot-starter/src/main/java/io/github/daggerok/distributed/lock/mongodb/autoconfigure/DistributedLockProperties.java
+++ distributed-lock-mongodb-spring-boot-starter/src/main/java/io/github/daggerok/distributed/lock/mongodb/autoconfigure/DistributedLockProperties.java
@@ -3,6 +3,7 @@ package io.github.daggerok.distributed.lock.mongodb.autoconfigure;
 import java.time.Duration;
 import lombok.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @Value
@@ -12,6 +13,7 @@ public class DistributedLockProperties {
     Boolean enabled;
     Duration lockPeriod;
 
+    @ConstructorBinding
     public DistributedLockProperties(@DefaultValue("true") Boolean enabled,
                                      @DefaultValue("15000ms") Duration lockPeriod) {
         this.enabled = enabled;
