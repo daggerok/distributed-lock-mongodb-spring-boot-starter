d417821edb598e107012f947e7c70ebf72a634a1
Maksim Kostromin <maksim@beachfront.com>
2023-04-05 02:11:57 -0400

    Add .bin/update-version.sh script to update project version up to specified SNAPSHOT.

--- .bin/central-release.sh
+++ .bin/central-release.sh
@@ -65,8 +65,8 @@ echo "Deploying ${VERSION} version..."
 if [[ "${VERSION}" == *"-SNAPSHOT" ]] ; then
   echo "Snapshot version was detected. Update ${VERSION} version to released, commit and push changes first..."
   cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}
-  cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:commit initialize
-  cd "${ROOT_PROJECT_DIR}" && VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+  cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:commit
+  cd "${ROOT_PROJECT_DIR}" && VERSION=$(bash mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
   cd "${ROOT_PROJECT_DIR}" && git add . ; git commit -am "Release ${VERSION} version." ; git push origin "${GIT_BRANCH}"
 fi
 echo "Deploying $VERSION release..."
@@ -78,7 +78,7 @@ cd "${ROOT_PROJECT_DIR}" && git tag "${VERSION}" ; git push origin "${VERSION}"
 echo "Create new snapshot version for next development iteration..."
 cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:set -DnewVersion=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.nextIncrementalVersion}-SNAPSHOT
 cd "${ROOT_PROJECT_DIR}" && ./mvnw build-helper:parse-version versions:commit
-cd "${ROOT_PROJECT_DIR}" && VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+cd "${ROOT_PROJECT_DIR}" && VERSION=$(bash mvnw help:evaluate -Dexpression=project.version -q -DforceStdout)
 cd "${ROOT_PROJECT_DIR}" && git add . ; git commit -am "Start next ${VERSION} development iteration." ; git push origin "${GIT_BRANCH}"
 
 echo "Released."
